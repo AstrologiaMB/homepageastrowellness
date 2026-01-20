a516993a611567e7ace34e549862565cb6be9429
Nicolas Arias <anonymmajora@gmail.com>
2026-01-20 17:35:15 -0500

    Added migration

---
+++ prisma/migrations/20260120_add_location_coordinates/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "birthLat" DOUBLE PRECISION,
+ADD COLUMN     "birthLon" DOUBLE PRECISION,
+ADD COLUMN     "residenceLat" DOUBLE PRECISION,
+ADD COLUMN     "residenceLon" DOUBLE PRECISION;
